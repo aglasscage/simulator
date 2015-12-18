f81374240fb31c9b94df97fb34e25d250a826640
Seth <seth@localhost.localdomain>
2015-12-18 02:42:39 -0500

    updating everything so far.

---
+++ includes/data.h
@@ -0,0 +1,172 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <sstream>
+
+class Label
+{
+public:
+	Label()
+		: line_number_(0), label_("")
+	{}
+	Label(const std::string label, const int line_number)
+		: label_(label), line_number_(line_number)
+	{}
+	std::string get_label() const
+	{
+		return label_;
+	}
+	int get_line_number() const
+	{
+		return line_number_;
+	}
+	bool operator==(const std::string label)
+	{
+		if (label_ == label) return true;
+		else return false;
+	}
+	bool operator!=(const std::string label)
+	{
+		if (label_ == label) return false;
+		else return true;
+	}
+	bool operator<=(const int & line_number)
+	{
+		if (line_number_ <= line_number) return true;
+		else return false;
+	}
+private:
+	std::string label_;
+	int line_number_;
+};
+
+class Data
+{
+public:
+	Data(int i=-9999, char c=' ', std::string s="", std::string label="")
+		: i_(i), c_(c), s_(s), label_(label)
+	{}
+	Data & operator=(const Data & data)
+	{
+		if (i_ == -9999 && s_ == "" && data.getChar() != ' ') 
+		{
+			c_ = data.getChar();
+		}
+		if (i_ == -9999 && c_ == ' ' && data.getString() != "")
+		{
+			s_ = data.getString();
+		}
+		else
+		{
+			std::cout << "ERROR: You are trying to store an invalid type.\n"
+					  << "ERROR: You must store values in the data segment according to their type"
+					  << std::endl;
+		}
+	}
+	void setData(const std::string & type, const std::string & data)
+	{
+		if (type == "asciiz")
+		{
+			setString(data);
+		}
+	}
+	void setInt(const int & i)
+	{
+		i_ = i;
+	}
+	void setChar(const int & c)
+	{
+		c_ = c;
+	}
+	void setString(const std::string & s)
+	{
+		s_ = s;
+	}
+	void setLabel(const std::string & label)
+	{
+		label_ = label;
+	}
+	int getInt() const
+	{
+		return i_;
+	}
+	char getChar() const
+	{
+		return c_;
+	}
+	std::string getString() const
+	{
+		return s_;
+	}
+	std::string getLabel() const
+	{
+		return label_;
+	}
+	void view()
+	{
+    	std::cout << getLabel() << ": ";
+    	if (getInt() != -9999) std::cout << getInt();
+		else if (getChar() != ' ') std::cout << getChar();
+		else if (getString() != "")
+		{
+			for (int i = 0; i < getString().size(); i++)
+			{
+				std::cout << getString()[i];
+			}
+		}
+	}
+private:
+	int i_;
+	char c_;
+	std::string s_;
+	std::string label_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const Data & data)
+{
+	if (data.getInt() != -9999) cout << data.getInt();
+	else if (data.getChar() != ' ') cout << data.getChar();
+	else if (data.getString() != "")
+	{
+		for (int i = 0; i < data.getString().size(); i++)
+		{
+			if (i < data.getString().size() - 1 && 
+				data.getString()[i] == '\\')
+			{
+				if (data.getString()[i + 1] == 'n')
+				{
+					std::cout << '\n';
+					i += 1;
+					continue;
+				}
+				else if (data.getString()[i + 1] == 't')
+				{
+					std::cout << '\t';
+					i += 1;
+					continue;
+				}
+			}
+			std::cout << data.getString()[i];
+		}
+	}
+	/*
+    cout << "Label:" << data.getLabel() << ", "
+    	 << "i:" << data.getInt() << ", "
+    	 << "c:" << data.getChar() << ", "
+    	 << "s:" << data.getString() << '\n';
+    */
+    
+    return cout;
+}
+
+std::ostream & operator<<(std::ostream & cout, const Label & label)
+{
+    cout << label.get_line_number() << ": " << label.get_label() << std::endl;
+    return cout;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,35 +4,74 @@
 #include "fileutil.h"
 #include "interpreter.h"
 
-const std::string NAME = "{Simulator Name}";
+const std::string NAME = "{Mips Sim}";
 
 int main()
 {
 	Interpreter interpreter;
 
-	std::cout << NAME << ": Please enter a filename. (Example: main.s)\n"
-			  << NAME << ": ";
-	//fileutil.open();
-	//interpreter.interpret(fileutil.getline());
 	std::string input;
-	std::getline(std::cin, input);
-	std::ifstream infile(input.c_str(), std::ios::in);
-	std::string file(input);
-	std::string line;
-	int line_num = 1;
-	if (infile.is_open())
+	std::cout << NAME << ": Please enter a command. (\"help\" for a list of commands)\n";
+	while (input != "q")
 	{
-		while (getline(infile, line))
+		std::cout << NAME << ": ";
+		std::getline(std::cin, input);
+		if (input == "help")
 		{
-			interpreter.add_instruction(line, line_num);
-			line_num++;
+			std::cout << "\n================Mips Sim================\n"
+					  << "\"reinitialize\" - Reinitialize Mips Sim.\n"
+					  << "\"open\" - Open a file in your Mips Sim folder.\n"
+					  << "\"run\" - Run the file opened by Mips Sim.\n"
+					  << "\"data\" - View the data segment.\n"
+					  << "\"edit\" - Enter edit mode.\n"
+					  << "\"quit\" - Exit Mips Sim.\n"
+					  << "========================================\n\n";
 		}
-		infile.close();
+		else if (input == "open")
+		{
+			std::cout << "\n\tPlease enter a file name. (Example: test.s)\n\n"
+				  	  << NAME << ": ";
+			std::getline(std::cin, input);
+			std::ifstream infile(input.c_str(), std::ios::in);
+			std::string file(input);
+			std::string line;
+			int line_num = 1;
+			if (infile.is_open())
+			{
+				while (getline(infile, line))
+				{
+					interpreter.add_instruction(line, line_num);
+					line_num++;
+				}
+				infile.close();
+				std::cout << "\n\tFile: \"" << input << "\" has been opened.\n\n";
+			}
+			else
+			{
+				std::cout << "\n\tCould not find file: \"" << input << "\".\n\n";
+			}
+		}
+		else if (input == "data")
+		{
+			std::cout << '\n';
+			interpreter.data_view();
+			std::cout << '\n';
+		}
+		else if (input == "reinitialize")
+		{
+			interpreter.reinitialize();
+			std::cout << "\n\tThe Interpreter has been reinitialized\n" << std::endl;
+		}
+		else if (input == "run")
+		{
+			interpreter.run();
+		}
+		else if (input == "") continue;
+		else
+		{
+			std::cout << "\n\tInvalid command. Please try again.\n" << std::endl;
+		}
 	}
-
-
-	interpreter.run();
-	//interpreter.print_data();
 
 	return 0;
 }
